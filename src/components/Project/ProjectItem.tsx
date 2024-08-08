95928f101d898d695f2fc0706b86ec0c1ddc4a61
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2024-08-08 19:16:55 +0900

    프로젝트 상세 설명 부분 수정

--- src/components/Project/ProjectItem.tsx
+++ src/components/Project/ProjectItem.tsx
@@ -42,7 +42,8 @@ const ProjectItem = ({
       </div>
       <div className="md:border-GRAY_LIGHT md:border-solid md:border-l-[1px] md:pl-4 markdown flex flex-col w-full gap-2">
         <div>
-          <blockquote className="whitespace-pre-wrap">{`${description}`}</blockquote>
+          //<blockquote className="whitespace-pre-wrap">{`${description}`}</blockquote>
+          <h3>`${description}`</h3>
           <div className="flex gap-1 flex-wrap">
             {stack.map((stack) => (
               <span
