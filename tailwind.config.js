f506ac0373920a6077ec2f518ffa377f850b9b20
Jinsun-Lee <68187536+Jinsun-Lee@users.noreply.github.com>
2024-08-08 16:35:00 +0900

    색상 변경 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,7 +33,7 @@ module.exports = {
         /**
          * @description 페이지 최상단 gradient의 시작 컬러입니다.
          */
-        GRADIENT_FROM: "#5183cf",
+        GRADIENT_FROM: "#9fc5e8",
 
         /**
          * @description 페이지 최상단 gradient의 종료 컬러입니다.
